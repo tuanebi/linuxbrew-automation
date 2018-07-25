4731b649c15c788cab63aa350ddd2e6b7c83b253
Sanjay Boddu <sboddu@ebi.ac.uk>
2018-07-25 12:36:19 -0400

    Checking if current directory is empty or not

---
+++ initiate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#echo $0;echo $1;echo $#;echo $@;echo $er;echo $?;if [ $? -eq 0 ];then echo "All OK"; else echo "something's wrong";fi; echo $$;
+
+
+
+
+#Check if current directory is empty
+
+if [ $(find . -type d | wc -l) -gt  1 ]
+  then
+    read -p "Current directory is not empty. Continue?: "  -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+      then
+        printf "\n%s\n" "Exiting"
+        exit 1
+     fi
+fi
+
+printf "\n%s\n" "Continuing..."
