64c8f191e99f36b89c358b61759251306526aa2a
Sanjay Boddu <sboddu@ebi.ac.uk>
2018-08-01 10:46:41 -0400

    git clones and taps

--- initiate.sh
+++ initiate.sh
@@ -3,6 +3,7 @@
 #echo $0;echo $1;echo $#;echo $@;echo $er;echo $?;if [ $? -eq 0 ];then echo "All OK"; else echo "something's wrong";fi; echo $$;
 
 
+#set -o xtrace
 
 #Check if current directory is empty
 
@@ -16,17 +17,27 @@ if [ $(find . -type d | wc -l) -gt  1 ]
      fi
 fi
 
-printf "\n%s\n" "Continuing..."
+printf "\n%s\n" "****** Continuing... ******"
 
-printf "\n%s\n" "Setting env variables..."
+printf "\n%s\n" "****** Setting env variables... ******"
 
-release_number="2018_06_05"
+release_number="2018_08_01"
 INSTALLATION_PATH="$PWD/$release_number"
 
 
+
+
+
+
+
+
+unset MANPATH INFOPATH SHARE_PATH HOMEBREW_ENSEMBL_MOONSHINE_ARCHIVE HTSLIB_DIR KENT_SRC MACHTYPE PERL5LIB
+export PATH="/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/sboddu/.local/bin:/home/sboddu/bin"
+
+
+
 env_variables=$(cat << EOF
-export PATH="$INSTALLATION_PATH/linuxbrew/bin:$PATH"
-export PATH="$INSTALLATION_PATH/paths/bin:$PATH"
+export PATH="$INSTALLATION_PATH/paths/bin:$INSTALLATION_PATH/linuxbrew/bin:$PATH"
 export MANPATH="$INSTALLATION_PATH/linuxbrew/share/man:$MANPATH"
 export INFOPATH="$INSTALLATION_PATH/linuxbrew/share/info:$INFOPATH"
 
@@ -64,6 +75,42 @@ fi
 
 
 eval "$env_variables"
+
+printf "\n%s\n" "****** Done setting env variables... ******"
+
+
+printf '\n%.0s' {1..5}
+
+
+printf "%s\n\n" "****** Creating $HOMEBREW_ENSEMBL_MOONSHINE_ARCHIVE dir ******"
+mkdir -p $HOMEBREW_ENSEMBL_MOONSHINE_ARCHIVE
+
+
+printf "\n\n%s\n\n" "****** Clonning linuxbrew into $INSTALLATION_PATH/linuxbrew ******"
+git clone https://github.com/Linuxbrew/brew.git $INSTALLATION_PATH/linuxbrew
+
+
+printf "\n\n%s\n\n" "****** Turning off brew analytics ******"
+brew analytics off
+
+printf "\n\n%s\n\n" "****** Clonning 1000G into $INSTALLATION_PATH/1000G-tools ******"
+git clone https://github.com/Ensembl/1000G-tools.git $INSTALLATION_PATH/1000G-tools
+
+
+#printf "\n\n%s\n" "****** Clonning ensembl git tools into $INSTALLATION_PATH/ensembl-git-tools ******"
+#git clone https://github.com/Ensembl/ensembl-git-tools.git $INSTALLATION_PATH/ensembl-git-tools
+
+
+printf "\n\n%s\n\n" "****** Tapping Homebrew External, Homebrew Ensembl, Homebrew Web, Homebrew Moonshine and Homebrew Cask ******"
+
+
+brew tap ensembl/external && \
+brew tap ensembl/ensembl && \
+brew tap ensembl/web && \
+brew tap ensembl/moonshine && \
+brew tap ensembl/cask
+
+
 
 
 
